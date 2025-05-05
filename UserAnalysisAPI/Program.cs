42ab08f33fab29134c3c9e96ab8c30ec8122bce5
belluccaz <lucas.bellucci@icloud.com>
2025-05-05 09:06:51 -0300

    fix: removendo alguns comentarios desnecessarios

--- UserAnalysisAPI/Program.cs
+++ UserAnalysisAPI/Program.cs
@@ -2,24 +2,20 @@ using UserAnalysisAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configura serviços normais aqui
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
-// Cria uma instância do UserService
 var userService = new UserService();
 
-// Aqui você define o caminho:
 await userService.LoadUsersAsync("./Data/usuarios_1000.json");
 
-// Registra o UserService para injeção de dependência (DI)
 builder.Services.AddSingleton(userService);
 
 var app = builder.Build();
 
-// Configura o pipeline
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
